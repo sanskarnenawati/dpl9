2a93f79783e4e4e881389c7752115fd53716d6af
Sanskar Nenawati <98727743+sanskarnenawati@users.noreply.github.com>
2023-04-29 09:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from predict_cost import predict
 import numpy as np
 import pandas as pd
-df  = pd.read_csv("rent.csv")
+df= pd.read_csv("rent.csv")
 st.title('Home price prediction')
 
 st.write('---')
@@ -16,10 +16,13 @@ bedroom = st.number_input('No. of bedrooms')
 # no. of balconies in the house
 status = st.radio('enter furnishing status', ('furnished',"unfurnished","semifurnished"))
 
-location = df['location'].drop_duplicates()
-loc_list = df["location"].tolist()
+# location = df['location'].drop_duplicates()
+p = dict()
+loc_list = df["location"].unique().tolist()
+for ind, loc in enumerate(loc_list):
+    p[loc] = ind
 location = st.selectbox(label = "Choose location", options = loc_list)
 if st.button('Predict House Price'):
-    cost = predict(np.array([[area, location, status, bedroom]]))
+    cost = predict(np.array([[area, p[location], status, bedroom]]))
     st.text(cost[0])
 
